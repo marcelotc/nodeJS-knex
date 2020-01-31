afa64198d6f459cac09d38f69bc9fda46935e276
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-01-31 18:57:44 -0300

    POSTing data to server/database

--- routes/api/todo.js
+++ routes/api/todo.js
@@ -3,10 +3,18 @@ const router = express.Router();
 const db = require('../../database');
 
 router.get('/', function (req, res) {
-    db.select().from('todo').then(function(data){
+    db.select().from('todo').then(function (data) {
         res.send(data);
     });
     //select * from "todo"
+});
+
+router.post('/', function (req, res) {
+    db.insert(req.body).returning('*').into('todo').then(function (data) {
+        res.send(data);
+    });
+    // select * from tablename(column1, column2) values(column_value, column2_value);
+    //select * from table where id = inserted_row;
 });
 
 module.exports = router;
