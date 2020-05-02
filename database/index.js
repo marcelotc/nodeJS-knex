b9fa086762f3c9dd6ec2142e8e5dc5e6698db559
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-05-01 21:29:58 -0300

    heroku

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const options = process.env.NODE_ENV === 'production' ?
     {
         client: 'pg',
-        connection: process.env.DATABASE_URL
+        connection: process.env.DATABASE_URL, searchPath: ['public']
     } : {
         client: 'pg',
         connection: {
--- index.js
+++ index.js
@@ -8,19 +8,19 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 app.use('/api', apiRoute);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-  
-    const path = require('path');
-    app.get('*', function (req, res) {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-  
-  const PORT = process.env.PORT || 8000;
-  app.listen(PORT);
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
